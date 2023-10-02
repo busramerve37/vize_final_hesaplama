2177fe8187e8ec67fc05abb422998f866e6afa99
busramerve37 <145451633+busramerve37@users.noreply.github.com>
2023-10-02 16:12:24 +0300

    Add files via upload

---
+++ button.ornek.15/button.ornek.15/button.ornek.15/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace button.ornek._15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblVize = new System.Windows.Forms.Label();
+            this.lblFinal = new System.Windows.Forms.Label();
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(258, 78);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 59);
+            this.btnHesapla.TabIndex = 0;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblVize
+            // 
+            this.lblVize.AutoSize = true;
+            this.lblVize.Location = new System.Drawing.Point(40, 78);
+            this.lblVize.Name = "lblVize";
+            this.lblVize.Size = new System.Drawing.Size(30, 13);
+            this.lblVize.TabIndex = 1;
+            this.lblVize.Text = "Vize:";
+            // 
+            // lblFinal
+            // 
+            this.lblFinal.AutoSize = true;
+            this.lblFinal.Location = new System.Drawing.Point(43, 110);
+            this.lblFinal.Name = "lblFinal";
+            this.lblFinal.Size = new System.Drawing.Size(32, 13);
+            this.lblFinal.TabIndex = 2;
+            this.lblFinal.Text = "Final:";
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Location = new System.Drawing.Point(46, 212);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(50, 13);
+            this.lblOrtalama.TabIndex = 3;
+            this.lblOrtalama.Text = "ortalama:";
+            // 
+            // txtVize
+            // 
+            this.txtVize.Location = new System.Drawing.Point(107, 70);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(100, 20);
+            this.txtVize.TabIndex = 4;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(107, 110);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(100, 20);
+            this.txtFinal.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 389);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtVize);
+            this.Controls.Add(this.lblOrtalama);
+            this.Controls.Add(this.lblFinal);
+            this.Controls.Add(this.lblVize);
+            this.Controls.Add(this.btnHesapla);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblVize;
+        private System.Windows.Forms.Label lblFinal;
+        private System.Windows.Forms.Label lblOrtalama;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.TextBox txtFinal;
+    }
+}
+
